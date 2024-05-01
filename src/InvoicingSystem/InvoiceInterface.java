674903568c964be65da4ebe5eda13f8ccbce7870
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-05-01 10:08:23 +0400

    Create interface for Invoice methods

---
+++ src/InvoicingSystem/InvoiceInterface.java
@@ -0,0 +1,19 @@
+package InvoicingSystem;
+
+import java.util.List;
+
+public interface InvoiceInterface {
+    Integer getInvoiceNumber();
+    String getCustomerName();
+
+    String getPhoneNumber();
+
+    // Get the list of items included in the invoice
+    List<Item> getItems();
+
+    Double getBalance();
+    Double calculateTotal();
+
+    void addItem(Item item);
+
+}
